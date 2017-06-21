5a95f965a537c39d02c3d01d5fdbc8fa0fc2af1b
Fabio Viola <desmovalvo@gmail.com>
2017-06-21 16:47:23 +0200

    Package created.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='sepy',
+      version='0.1',
+      description=' Client-side libraries for the SEPA platform (Python3) ',
+      url='https://github.com/arces-wot/SEPA-python3-APIs',
+      author='Fabio Viola',
+      author_email='fabio.viola@unibo.it',
+      license='GPLv3',
+      packages=['sepy'],
+      install_requires=[
+          "websocket"
+      ],
+      zip_safe=False)

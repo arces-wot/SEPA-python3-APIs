5638c2e87193fdbe1bfc8c2849b472b507439a6c
Francesco Antoniazzi <francesco.antoniazzi1991@gmail.com>
2020-05-17 18:55:59 +0200

    change version number

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md","r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='sepy',
-      version='0.3',
+      version='0.42',
       description=' Client-side libraries for the SEPA platform (Python3) ',
       long_description=long_description,
       long_description_content_type="text/markdown",
-      url='https://github.com/arces-wot/SEPA-python3-APIs',
+      url='https://github.com/fr4ncidir/SEPA-python3-APIs',
       author='Fabio Viola, Francesco Antoniazzi',
-      author_email='fabio.viola@unibo.it, francesco.antoniazzi@unibo.it',
+      author_email='fabio.viola@unibo.it, francesco.antoniazzi1991@gmail.com',
       packages=setuptools.find_packages(),
       license='GPLv3',
       test_suite="sepy.tests",
